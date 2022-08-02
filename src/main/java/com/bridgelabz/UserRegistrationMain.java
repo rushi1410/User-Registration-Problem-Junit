0bd7993da80e403ce9552d6c29ad822fe92a725f
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-08-02 10:46:18 +0530

    Added:Enter A Valid Last Name

--- src/main/java/com/bridgelabz/UserRegistrationMain.java
+++ src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -19,6 +19,23 @@ public class UserRegistrationMain {
 			System.err.println(FirstName+" is an Invalid First Name");
 
 	}
+	public static void lastNameValidation(String lastName) {
+
+		boolean isLastName;
+		String lastNameRegex = "[A-Z]{1}[a-z]{2,}$";
+		Pattern patternObj = Pattern.compile(lastNameRegex);
+		if (lastName == null) {
+			isLastName = false;
+		}
+		Matcher matcherObj = patternObj.matcher(lastName);
+		isLastName = matcherObj.matches();
+
+		if (isLastName)
+			System.out.println(lastName + " is an Valid Last Name");
+		else
+			System.err.println(lastName + " is an Invalid Last Name");
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration System");
 		Scanner input = new Scanner(System.in);
@@ -26,7 +43,11 @@ public class UserRegistrationMain {
 		System.out.print("Enter firstname: ");
 		String FirstName = input.nextLine();
 		firstNameValidation(FirstName);
+
+		System.out.println("Enter your lastname: ");
+		String LastName = input.nextLine();
+		lastNameValidation(LastName);
 		input.close();
-	}	
+	}
 }
 
