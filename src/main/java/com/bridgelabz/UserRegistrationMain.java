46d5a00aade9c464382a5839d06c4e595618ce7e
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-08-02 10:37:31 +0530

    Added:Enter A Valid First Name

--- src/main/java/com/bridgelabz/UserRegistrationMain.java
+++ src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -1,7 +1,33 @@
 package com.bridgelabz;
-
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class UserRegistrationMain {
-	public static void main(String[] args) {
-		System.out.println("Welcome To User Registration Problem");
+
+	public static  void firstNameValidation(String FirstName){
+		boolean isFirstName;
+		String regex = "^[A-Z]{1}[a-z A-Z]{2,}$";
+		Pattern patternObj = Pattern.compile(regex);
+		if (FirstName == null){
+
+		}
+		Matcher matcherObj = patternObj.matcher(FirstName);
+		isFirstName =  matcherObj.matches();
+		if(isFirstName)
+			System.out.println(FirstName+" is an Valid First Name");
+		else
+			System.err.println(FirstName+" is an Invalid First Name");
+
 	}
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration System");
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter firstname: ");
+		String FirstName = input.nextLine();
+		firstNameValidation(FirstName);
+
+		input.close();
+	}	
 }
+
